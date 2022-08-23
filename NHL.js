c678554d53e1d432a6dc74009cc1e10c41cc7bfe
DLindeblom <drdlindeblom@gmail.com>
2022-08-23 09:28:01 -0400

    create basic NHL structure

---
+++ NHL.js
@@ -0,0 +1,48 @@
+const NHL = {
+  Eastern: {
+    Metropolitan: {
+      "Hurricanes": {},
+      "Rangers": {},
+      "Penguins": {},
+      "Capitals": {},
+      "Islanders": {},
+      "Blue Jackets": {},
+      "Devils": {},
+      "Flyers": {}
+    },
+
+    Atlantic: {
+      "Panthers": {}, 
+      "Maple Leafs": {}, 
+      "Lightning": {}, 
+      "Bruins": {}, 
+      "Sabres": {},
+      "Red Wings": {},
+      "Senators": {},
+      "Canadiens": {}
+    }
+  },
+  Western: {
+    Central: {
+      "Avalance": {}, 
+      "Wild": {},
+      "Blues": {}, 
+      "Stars": {},
+      "Predators": {},
+      "Jets": {},
+      "Blackhawks": {}, 
+      "Coyotes": {}
+    },
+
+    Pacific: {
+      "Flames": {},
+      "Oilers": {},
+      "Kings": {},
+      "Golden Knights": {},
+      "Canucks": {},
+      "Sharks": {},
+      "Ducks": {},
+      "Kraken": {}
+    }
+  }
+}
