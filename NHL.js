aa0daaff09b4464397536604b551f7a7f0b5e54c
DLindeblom <drdlindeblom@gmail.com>
2022-08-23 11:19:21 -0400

    populate data for atlantic division

--- NHL.js
+++ NHL.js
@@ -2,211 +2,233 @@ const NHL = {
   Eastern: {
     Metropolitan: {
       "Hurricanes": {
-        city: "",
-        country: "",
+        city: "Carolina",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "PNC Arena",
+          location: "Raleigh, North Carolina",
+          capacity: 18680
         },
-        founded: 0,
+        founded: 1972,
+        formerly: ["New England Whalers", "Hartford Whalers"],
+        cups: 1,
+        cupYears: [2006]
+      },
+      "Rangers": {
+        city: "New York City",
+        country: "United States",
+        stadium: {
+          name: "Madison Square Garden",
+          city: "New York City, New York",
+          capacity: 18006
+        },
+        founded: 1926,
+        formerly: [],
+        cups: 4,
+        cupYears: [1928, 1933, 1940, 1994]
+      },
+      "Penguins": {
+        city: "Pittsburgh",
+        country: "United States",
+        stadium: {
+          name: "PPG Paints Arena",
+          city: "Pittsburgh, Pennsylvania",
+          capacity: 19758
+        },
+        founded: 1967,
+        cups: 5,
+        cupYears: [1991, 1992, 2009, 2016, 2017]
+      },
+      "Capitals": {        
+        city: "Washington",
+        country: "United States",
+        stadium: {
+          name: "Capital One Arena",
+          city: "Washington, D.C.",
+          capacity: 20356
+        },
+        founded: 1974,
+        cups: 1,
+        cupYears: [2018]
+      },
+      "Islanders": {        
+        city: "New York",
+        country: "United States",
+        stadium: {
+          name: "UBS Arena",
+          city: "Elmont, New York",
+          capacity: 17255
+        },
+        founded: 1972,
+        cups: 4,
+        cupYears: [1980, 1981, 1982, 1983]
+      },
+      "Blue Jackets": {        
+        city: "Columbus",
+        country: "United States",
+        stadium: {
+          name: "Nationwide Arena",
+          city: "Columbus, Ohio",
+          capacity: 20000
+        },
+        founded: 1997,
         cups: 0,
         cupYears: []
       },
-      "Rangers": {
-        city: "",
-        country: "",
-        stadium: {
-          name: "",
-          city: "",
-          capacity: 0
-        },
-        founded: 0,
-        cups: 0,
-        cupYears: []},
-      "Penguins": {
-        city: "",
-        country: "",
-        stadium: {
-          name: "",
-          city: "",
-          capacity: 0
-        },
-        founded: 0,
-        cups: 0,
-        cupYears: []},
-      "Capitals": {        
-        city: "",
-        country: "",
-        stadium: {
-          name: "",
-          city: "",
-          capacity: 0
-        },
-        founded: 0,
-        cups: 0,
-        cupYears: []},
-      "Islanders": {        
-        city: "",
-        country: "",
-        stadium: {
-          name: "",
-          city: "",
-          capacity: 0
-        },
-        founded: 0,
-        cups: 0,
-        cupYears: []},
-      "Blue Jackets": {        
-        city: "",
-        country: "",
-        stadium: {
-          name: "",
-          city: "",
-          capacity: 0
-        },
-        founded: 0,
-        cups: 0,
-        cupYears: []},
       "Devils": {        
-        city: "",
-        country: "",
+        city: "New Jersey",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Prudential Center",
+          city: "Newark, New Jersey",
+          capacity: 19500
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []},
+        founded: 1974,
+        formerly: ["Kansas City Scouts, Colorado Rockies"],
+        cups: 3,
+        cupYears: [1995, 2000, 2003]
+      },
       "Flyers": {        
-        city: "",
-        country: "",
+        city: "Philadelphia",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Wells Fargo Center",
+          city: "Philadelphia, Pennsylvania",
+          capacity: 19500
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []}
+        founded: 1967,
+        cups: 2,
+        cupYears: [1974, 1975]
+      }
     },
 
     Atlantic: {
       "Panthers": {        
-        city: "",
-        country: "",
+        city: "Florida",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "FLA Live Arena",
+          city: "Sunrise, Florida",
+          capacity: 20737
         },
-        founded: 0,
+        founded: 1993,
         cups: 0,
-        cupYears: []}, 
+        cupYears: []
+      }, 
       "Maple Leafs": {        
-        city: "",
-        country: "",
+        city: "Toronto",
+        country: "Canada",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Scotiabank Arena",
+          city: "Toronto",
+          capacity: 19800,
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []}, 
+        founded: 1917,
+        formerly: ["Toronto Arenas", "Toronto St. Patricks"],
+        cups: 13,
+        cupYears: [1918, 1922, 1932, 1942, 1945, 1947, 1948, 1949, 1951, 1962, 1963, 1967]}, 
       "Lightning": {        
-        city: "",
-        country: "",
+        city: "Tampa Bay",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Amalie Arena",
+          city: "Tampa",
+          capacity: 21500
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []}, 
+        founded: 1992,
+        cups: 3,
+        cupYears: [2004, 2020, 2021]
+      }, 
       "Bruins": {        
-        city: "",
-        country: "",
+        city: "Boston",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "TD Garden Arena",
+          city: "Boston, Massachusetts",
+          capacity: 19580
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []}, 
+        founded: 1924,
+        cups: 6,
+        cupYears: [1929, 1939, 1941, 1970, 1972, 2011]
+      }, 
       "Sabres": {        
-        city: "",
-        country: "",
+        city: "Buffalo",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "KeyBank Center",
+          city: "Buffalo, New York",
+          capacity: 19200,
         },
-        founded: 0,
+        founded: 1970,
         cups: 0,
-        cupYears: []},
+        cupYears: []
+      },
       "Red Wings": {        
-        city: "",
-        country: "",
+        city: "Detroit",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Little Caesars Arena",
+          city: "Detroit, Michigan",
+          capacity: 19515,
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []},
+        founded: 1926,
+        formerly: ["Detroit Cougars"],
+        cups: 11,
+        cupYears: [1936, 1937, 1943, 1950, 1952, 1954, 1955, 1997, 1998, 2002, 2008]
+      },
       "Senators": {        
-        city: "",
-        country: "",
+        city: "Ottawa",
+        country: "Canada",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Canadian Tire Centre",
+          city: "Kanata, Ontario",
+          capacity: 19153,
         },
-        founded: 0,
+        founded: 1992,
         cups: 0,
-        cupYears: []},
+        cupYears: []
+      },
       "Canadiens": {        
-        city: "",
-        country: "",
+        city: "Montreal",
+        country: "Canada",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Centre Bell",
+          city: "Montreal, Quebec",
+          capacity: 21302
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []}
+        founded: 1910,
+        cups: 24,
+        cupYears: [1916, 1924, 1930, 1931, 1944, 1946, 1953, 1956, 1957, 1958, 1959, 1960, 1965, 1966, 1968, 1969, 1971, 1973, 1976, 1977, 1978, 1979, 1986, 1993]
+      }
     }
   },
   Western: {
     Central: {
       "Avalance": {        
-        city: "",
-        country: "",
+        city: "Colorado",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Ball Arena",
+          city: "Denver, Colorado",
+          capacity: 20000,
         },
-        founded: 0,
-        cups: 0,
-        cupYears: []}, 
+        founded: 1972,
+        formerly: ["Quebec Nordiques"],
+        cups: 3,
+        cupYears: [1996, 2001, 2022]
+      }, 
       "Wild": {        
-        city: "",
-        country: "",
+        city: "Minnesota",
+        country: "United States",
         stadium: {
-          name: "",
-          city: "",
-          capacity: 0
+          name: "Xcel Energy Center",
+          city: "Saint Paul, Minnesota",
+          capacity: 20554
         },
-        founded: 0,
+        founded: 2000,
         cups: 0,
-        cupYears: []},
+        cupYears: []
+      },
       "Blues": {        
         city: "",
         country: "",
