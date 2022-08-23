dd092a390ce4748c01ba2ee29938f048139d45c6
DLindeblom <drdlindeblom@gmail.com>
2022-08-23 09:45:43 -0400

    add information structure to nhl and nfl

--- NFL.js
+++ NFL.js
@@ -1,61 +1,413 @@
 const NFL = {
   NFC: {
     East: {
-      "Giants": {},
-      "Eagles": {},
-      "Cowboys": {},
-      "Commanders": {}
+      "Giants": {
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Eagles": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Cowboys": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Commanders": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
     },
 
     North: {
-      "Bears": {},
-      "Packers": {},
-      "Lions": {},
-      "Vikings": {}
+      "Bears": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Packers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Lions": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Vikings": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
     },
 
     South: {
-      "Falcons": {},
-      "Panthers": {},
-      "Saints": {},
-      "Buccaneers": {}
-    }, 
+      "Falcons": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Panthers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Saints": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Buccaneers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
+    },
 
     West: {
-      "49ers": {},
-      "Cardinals": {},
-      "Rams": {},
-      "Seahawks": {}
+      "49ers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Cardinals": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Rams": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Seahawks": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
     }
-  }, 
+  },
 
   AFC: {
     East: {
-      "Bills": {},
-      "Jets": {},
-      "Patriots": {}, 
-      "Dolphins": {}
+      "Bills": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Jets": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Patriots": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Dolphins": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
     },
 
     North: {
-      "Ravens": {},
-      "Steelers": {}, 
-      "Browns": {}, 
-      "Bengals": {}
-    }, 
+      "Ravens": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Steelers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Browns": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Bengals": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
+    },
 
     South: {
-      "Texans": {},
-      "Titans": {}, 
-      "Colts": {},
-      "Jaguars": {}
+      "Texans": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Titans": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Colts": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Jaguars": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
     },
 
     West: {
-      "Raiders": {},
-      "Broncos": {},
-      "Chiefs": {}, 
-      "Chargers": {}
+      "Raiders": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Broncos": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Chiefs": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      },
+      "Chargers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        SuperBowls: 0,
+        SuperBowlYears: []
+      }
     }
   }
-}
+};
--- NHL.js
+++ NHL.js
@@ -1,48 +1,369 @@
 const NHL = {
   Eastern: {
     Metropolitan: {
-      "Hurricanes": {},
-      "Rangers": {},
-      "Penguins": {},
-      "Capitals": {},
-      "Islanders": {},
-      "Blue Jackets": {},
-      "Devils": {},
-      "Flyers": {}
+      "Hurricanes": {
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []
+      },
+      "Rangers": {
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Penguins": {
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Capitals": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Islanders": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Blue Jackets": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Devils": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Flyers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}
     },
 
     Atlantic: {
-      "Panthers": {}, 
-      "Maple Leafs": {}, 
-      "Lightning": {}, 
-      "Bruins": {}, 
-      "Sabres": {},
-      "Red Wings": {},
-      "Senators": {},
-      "Canadiens": {}
+      "Panthers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}, 
+      "Maple Leafs": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}, 
+      "Lightning": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}, 
+      "Bruins": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}, 
+      "Sabres": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Red Wings": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Senators": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Canadiens": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}
     }
   },
   Western: {
     Central: {
-      "Avalance": {}, 
-      "Wild": {},
-      "Blues": {}, 
-      "Stars": {},
-      "Predators": {},
-      "Jets": {},
-      "Blackhawks": {}, 
-      "Coyotes": {}
+      "Avalance": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}, 
+      "Wild": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Blues": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}, 
+      "Stars": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Predators": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Jets": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Blackhawks": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}, 
+      "Coyotes": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}
     },
 
     Pacific: {
-      "Flames": {},
-      "Oilers": {},
-      "Kings": {},
-      "Golden Knights": {},
-      "Canucks": {},
-      "Sharks": {},
-      "Ducks": {},
-      "Kraken": {}
+      "Flames": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Oilers": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Kings": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Golden Knights": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Canucks": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Sharks": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Ducks": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []},
+      "Kraken": {        
+        city: "",
+        country: "",
+        stadium: {
+          name: "",
+          city: "",
+          capacity: 0
+        },
+        founded: 0,
+        cups: 0,
+        cupYears: []}
     }
   }
 }
