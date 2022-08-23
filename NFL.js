37fba1414d57f1940ed7bf023562234041757477
DLindeblom <drdlindeblom@gmail.com>
2022-08-23 09:18:08 -0400

    create basic NFL structure

---
+++ NFL.js
@@ -0,0 +1,61 @@
+const NFL = {
+  NFC: {
+    East: {
+      "Giants": {},
+      "Eagles": {},
+      "Cowboys": {},
+      "Commanders": {}
+    },
+
+    North: {
+      "Bears": {},
+      "Packers": {},
+      "Lions": {},
+      "Vikings": {}
+    },
+
+    South: {
+      "Falcons": {},
+      "Panthers": {},
+      "Saints": {},
+      "Buccaneers": {}
+    }, 
+
+    West: {
+      "49ers": {},
+      "Cardinals": {},
+      "Rams": {},
+      "Seahawks": {}
+    }
+  }, 
+
+  AFC: {
+    East: {
+      "Bills": {},
+      "Jets": {},
+      "Patriots": {}, 
+      "Dolphins": {}
+    },
+
+    North: {
+      "Ravens": {},
+      "Steelers": {}, 
+      "Browns": {}, 
+      "Bengals": {}
+    }, 
+
+    South: {
+      "Texans": {},
+      "Titans": {}, 
+      "Colts": {},
+      "Jaguars": {}
+    },
+
+    West: {
+      "Raiders": {},
+      "Broncos": {},
+      "Chiefs": {}, 
+      "Chargers": {}
+    }
+  }
+}
