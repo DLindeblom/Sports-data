e50844d98573ecbc9dbefb845fb694d20e786509
DLindeblom <drdlindeblom@gmail.com>
2022-08-23 14:01:33 -0400

    removed cup count

--- NHL.js
+++ NHL.js
@@ -11,7 +11,6 @@ const NHL = [
     },
     founded: 1972,
     formerly: ["New England Whalers", "Hartford Whalers"],
-    cups: 1,
     cupYears: [2006]
   },
   {
@@ -26,7 +25,6 @@ const NHL = [
     },
     founded: 1926,
     formerly: [],
-    cups: 4,
     cupYears: [1928, 1933, 1940, 1994]
   },
   {
@@ -40,7 +38,6 @@ const NHL = [
       capacity: 19758
     },
     founded: 1967,
-    cups: 5,
     cupYears: [1991, 1992, 2009, 2016, 2017]
   },
   {
@@ -54,7 +51,6 @@ const NHL = [
       capacity: 20356
     },
     founded: 1974,
-    cups: 1,
     cupYears: [2018]
   },
   {
@@ -68,7 +64,6 @@ const NHL = [
       capacity: 17255
     },
     founded: 1972,
-    cups: 4,
     cupYears: [1980, 1981, 1982, 1983]
   },
   {
@@ -82,7 +77,6 @@ const NHL = [
       capacity: 20000
     },
     founded: 1997,
-    cups: 0,
     cupYears: []
   },
   {
@@ -97,7 +91,6 @@ const NHL = [
     },
     founded: 1974,
     formerly: ["Kansas City Scouts, Colorado Rockies"],
-    cups: 3,
     cupYears: [1995, 2000, 2003]
   },
   {
@@ -111,7 +104,6 @@ const NHL = [
       capacity: 19500
     },
     founded: 1967,
-    cups: 2,
     cupYears: [1974, 1975]
   },
   {
@@ -125,7 +117,6 @@ const NHL = [
       capacity: 20737
     },
     founded: 1993,
-    cups: 0,
     cupYears: []
   },
   {
@@ -140,7 +131,6 @@ const NHL = [
     },
     founded: 1917,
     formerly: ["Toronto Arenas", "Toronto St. Patricks"],
-    cups: 13,
     cupYears: [1918, 1922, 1932, 1942, 1945, 1947, 1948, 1949, 1951, 1962, 1963, 1967]
   },
   {
@@ -154,7 +144,6 @@ const NHL = [
       capacity: 21500
     },
     founded: 1992,
-    cups: 3,
     cupYears: [2004, 2020, 2021]
   },
   {
@@ -168,7 +157,6 @@ const NHL = [
       capacity: 19580
     },
     founded: 1924,
-    cups: 6,
     cupYears: [1929, 1939, 1941, 1970, 1972, 2011]
   },
   {
@@ -182,7 +170,6 @@ const NHL = [
       capacity: 19200,
     },
     founded: 1970,
-    cups: 0,
     cupYears: []
   },
   {
@@ -197,7 +184,6 @@ const NHL = [
     },
     founded: 1926,
     formerly: ["Detroit Cougars"],
-    cups: 11,
     cupYears: [1936, 1937, 1943, 1950, 1952, 1954, 1955, 1997, 1998, 2002, 2008]
   },
   {
@@ -211,7 +197,6 @@ const NHL = [
       capacity: 19153,
     },
     founded: 1992,
-    cups: 0,
     cupYears: []
   },
   {
@@ -225,7 +210,6 @@ const NHL = [
       capacity: 21302
     },
     founded: 1910,
-    cups: 24,
     cupYears: [1916, 1924, 1930, 1931, 1944, 1946, 1953, 1956, 1957, 1958, 1959, 1960, 1965, 1966, 1968, 1969, 1971, 1973, 1976, 1977, 1978, 1979, 1986, 1993]
   },
   {
@@ -240,7 +224,6 @@ const NHL = [
     },
     founded: 1972,
     formerly: ["Quebec Nordiques"],
-    cups: 3,
     cupYears: [1996, 2001, 2022]
   },
   {
@@ -254,7 +237,6 @@ const NHL = [
       capacity: 20554
     },
     founded: 2000,
-    cups: 0,
     cupYears: []
   },
   {
@@ -268,7 +250,6 @@ const NHL = [
       capacity: 19260
     },
     founded: 1967,
-    cups: 1,
     cupYears: [2019]
   },
   {
@@ -283,7 +264,6 @@ const NHL = [
     },
     founded: 1967,
     formerly: ["Minnesota North Stars"],
-    cups: 1,
     cupYears: [1999]
   },
   {
@@ -297,7 +277,6 @@ const NHL = [
       capacity: 19891,
     },
     founded: 1998,
-    cups: 0,
     cupYears: []
   },
   {
@@ -312,7 +291,6 @@ const NHL = [
     },
     founded: 1999,
     formerly: ["Atlanta Thrashers"],
-    cups: 0,
     cupYears: []
   },
   {
@@ -327,7 +305,6 @@ const NHL = [
     },
     founded: 1926,
     formerly: ["Chicago Black Hawks"],
-    cups: 6,
     cupYears: [1934, 1938, 1961, 2010, 2013, 2015]
   },
   {
@@ -342,7 +319,6 @@ const NHL = [
     },
     founded: 1972,
     formerly: ["Winnipeg Jets", "Phoenix Coyotes"],
-    cups: 0,
     cupYears: []
   },
   {
@@ -356,7 +332,6 @@ const NHL = [
       capacity: 19289
     },
     founded: 1972,
-    cups: 1,
     cupYears: [1989]
   },
   {
@@ -371,7 +346,6 @@ const NHL = [
     },
     founded: 1972,
     formerly: ["Alberta Oilers"],
-    cups: 5,
     cupYears: [1984, 1985, 1987, 1988, 1990]
   },
   {
@@ -385,7 +359,6 @@ const NHL = [
       capacity: 20000
     },
     founded: 1966,
-    cups: 2,
     cupYears: [2012, 2014]
   },
   {
@@ -399,7 +372,6 @@ const NHL = [
       capacity: 17500
     },
     founded: 2017,
-    cups: 0,
     cupYears: []
   },
   {
@@ -413,7 +385,6 @@ const NHL = [
       capacity: 18910
     },
     founded: 1945,
-    cups: 0,
     cupYears: []
   },
   {
@@ -427,7 +398,6 @@ const NHL = [
       capacity: 17562
     },
     founded: 1991,
-    cups: 0,
     cupYears: []
   },
   {
@@ -442,7 +412,6 @@ const NHL = [
     },
     founded: 1993,
     formerly: ["Mighty Ducks of Anaheim"],
-    cups: 1,
     cupYears: [2007]
   },
   {
@@ -456,7 +425,6 @@ const NHL = [
       capacity: 17459
     },
     founded: 2018,
-    cups: 0,
     cupYears: []
   }
 ];
